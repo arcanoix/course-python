1e97ea4508983ca3dbedbd659d385e9f5fa7c968
thavoo <gustavoh.2312@gmail.com>
2018-11-01 19:43:09 -0500

    agregando clase 1

---
+++ clase1/imprimir.py
@@ -0,0 +1,10 @@
+def imprimir():
+	print("Hola Mundo")
+	print("Esto es una funcion")
+	print("Bienvenido al Curso Python")
+	print("--------------------------")
+
+
+
+#llamando una funcion
+imprimir()
