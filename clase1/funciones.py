9b34f4def1f88445b6357f9926dd625cf0c395de
thavoo <gustavoh.2312@gmail.com>
2018-11-02 16:53:32 -0500

    funciones basicas

---
+++ clase1/funciones.py
@@ -0,0 +1,22 @@
+def suma():
+	numero1= 2
+	numero2= 3
+	total = numero1+numero2
+
+	return total
+	#print("la suma es: ", total)
+
+def multiplicacion(n1, n2):
+	total = n1*n2
+	return total
+	#print("La multiplicacion es: ", total)
+
+
+
+
+
+#estamos llamando una funcion sin parametros
+print(suma())
+
+#llamamos una funcion con parametros
+print(multiplicacion(5,2))
